e610ae0f3b046718190bf74e46a9db1a85bcbe96
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-06-17 17:34:04 +0300

    transaction

--- bank.py
+++ bank.py
@@ -5,6 +5,7 @@ class Account:
         self.balance = 0
         self.deposits = []
         self.withdrawals = []
+        self.transaction = 100
         
     def  deposit(self,amount):
         if amount <=0:
@@ -14,8 +15,7 @@ class Account:
              self.deposits.append(amount) 
              return f"Hello {self.accname}, your deposit is {amount} and your new balance is {self.balance} and your deposits are {self.deposits}" 
     def withdrawal(self,amount):
-        self.transaction = 100
-        if amount > self.balance:
+        if amount+self.transaction > self.balance:
             return f"Hello {self.accname}, your balance is {self.balance} you can't withdraw {amount}"    
         elif amount <=0:
             return f"Withdrawal amount must be greater than 0"
@@ -24,12 +24,12 @@ class Account:
             self.withdrawals.append(amount)
             return f"Hello {self.accname}, your withdrawal is {amount} and your new balance is {self.balance} and the withdrawals are {self.withdrawals}"
     def deposit_statements(self):
-         for a in self.deposits:
-             print(a, end="\n")
+         for deposit in self.deposits:
+             print(deposit, end="\n")
              
     def withdrawals_statement(self):
-        for b in self.withdrawals:
-            print(b,end="\n")
+        for withdrawal in self.withdrawals:
+            print(withdrawal,end="\n")
 
     def current_balance(self):
         return f"{self.balance}"        
